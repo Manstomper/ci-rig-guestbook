c5e020cfd82fe7b5d0060e39f15d5bf7c9d9e587
Manstomper <7th.rat@gmail.com>
2014-03-17 22:09:31 +0200

    Minor update

--- application/views/templates/guestbook_footer.php
+++ application/views/templates/guestbook_footer.php
@@ -4,7 +4,7 @@
 
 <ul id="footer">
 <li>Powered by <a href="http://ellislab.com/codeigniter">CodeIgniter</a></li>
-<li>Script by <a href="http://manstomper.com">Riina Granö</a></li>
+<li><a href="http://manstomper.com">Manstomper.com</a></li>
 <li><a href="http://adamwhitcroft.com/batch/">Batch</a> icons</li>
 <li><a href="http://creativecommons.org/licenses/by-sa/3.0">CC3.0 Licence</a></li>
 </ul>
